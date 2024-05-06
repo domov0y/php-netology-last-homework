e29ce871c23ca26b76361346ccfd274def430915
domov0y <79624187+domov0y@users.noreply.github.com>
2024-05-06 15:58:38 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,15 @@
 <?php
-
-echo 'I\'m alive!';
+session_start();
+?>
+<html>
+ <?php 
+if (!isset( $_SESSION['whoareyou']))
+  include_once("form.php");
+else
+  echo "Доброго времени суток <b>".$_SESSION['whoareyou']."</b> <a href=/exit.php>Exit</a>";
+?>
+<hr>
+<?php
+  include_once("manifest-of-hackers.php");
+  ?>
+</html>
